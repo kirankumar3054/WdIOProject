dd943d89c1717d540c99359869d20eb5a0424953
Kiran3054 <kirankumar3054@gmail.com>
2021-06-02 22:40:04 -0400

    Reports file format changed to xml

--- wdio.conf.js
+++ wdio.conf.js
@@ -62,7 +62,6 @@ exports.config = {
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
         maxInstances: 5,
-        //
         browserName: 'chrome',
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
@@ -123,8 +122,8 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['browserstack'],
-    // services: ['selenium-standalone'],
+    // services: ['browserstack'],
+    services: ['selenium-standalone'],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
@@ -149,7 +148,10 @@ exports.config = {
     reporters: [
         [
             'junit', {
-                outputDir: 'Reports-Junit'
+                outputDir: './Reports',
+                outputFileFormat: function (options) {
+                    return `results-${new Date().getTime()}.xml`;
+                },
             },
         ],
     ],
